7e36844da555415ebaaed871a1426a43edcf9a0d
huakang <741180788@qq.com>
2022-07-15 16:52:07 +0800

    init

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom";
+import "@/styles/reset.less";
+import "antd/dist/antd.less";
+import App from "@/App";
+
+ReactDOM.render(
+	<App />,
+	document.getElementById("root")
+);
---
+++ vite.config.ts
@@ -0,0 +1,77 @@
+import { defineConfig, loadEnv, ConfigEnv, UserConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import eslintPlugin from "vite-plugin-eslint";
+
+// @see: https://vitejs.dev/config/
+export default defineConfig((mode: ConfigEnv): UserConfig => {
+	const env = loadEnv(mode.mode, process.cwd());
+
+	return {
+		// base: "/",
+		// alias config
+		resolve: {
+			alias: {
+				"@": resolve(__dirname, "./src")
+			}
+		},
+		// global css
+		css: {
+			preprocessorOptions: {
+				less: {
+					// modifyVars: {
+					// 	"primary-color": "#1DA57A",
+					// },
+					javascriptEnabled: true,
+					additionalData: `@import "@/styles/var.less";`
+				}
+			}
+		},
+		// server config
+		server: {
+			host: "0.0.0.0", // 服务器主机名，如果允许外部访问，可设置为"0.0.0.0"
+			port: 3000,
+			open: true,
+			cors: true
+			// https: false,
+			// 代理跨域（mock 不需要配置，这里只是个事列）
+			// proxy: {
+			// 	"/api": {
+			// 		target: "", // easymock
+			// 		changeOrigin: true,
+			// 		rewrite: path => path.replace(/^\/api/, "")
+			// 	}
+			// }
+		},
+		// plugins
+		plugins: [
+			react(),
+			// * EsLint 报错信息显示在浏览器界面上
+			eslintPlugin()
+		],
+		esbuild: {
+			pure: ["console.log", "debugger"]
+		},
+		// build configure
+		build: {
+			outDir: "dist",
+			// esbuild 打包更快，但是不能去除 console.log，去除 console 使用 terser 模式
+			minify: "esbuild",
+			// minify: "terser",
+			// terserOptions: {
+			// 	compress: {
+			// 		drop_console: viteEnv.VITE_DROP_CONSOLE,
+			// 		drop_debugger: true
+			// 	}
+			// },
+			rollupOptions: {
+				output: {
+					// Static resource classification and packaging
+					chunkFileNames: "assets/js/[name]-[hash].js",
+					entryFileNames: "assets/js/[name]-[hash].js",
+					assetFileNames: "assets/[ext]/[name]-[hash].[ext]"
+				}
+			}
+		}
+	};
+});
